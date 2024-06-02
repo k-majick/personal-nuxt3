dc966377d193a1d84d2af539be2bf8a1ce2dab03
k-majick <k-majick>
2024-06-02 21:31:58 +0200

    dialog

---
+++ components/Dialog.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="dialog__mask" :class="`dialog__mask--${dialogType}`">
+    <div class="dialog__table">
+      <div class="dialog__cell">
+        <div
+          class="dialog" 
+          :class="`dialog--${uiStore.currentTheme}`"
+        >
+          <button class="dialog__close" type="button" @click="$emit('close')">
+            &times;
+          </button>
+          <slot name="header"></slot>
+          <slot name="content"></slot>
+          <slot name="action"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useUiStore } from "@/store/ui";
+
+const uiStore = useUiStore();
+
+defineProps({
+  dialogType: {
+    type: String,
+    default: "",
+  },
+  id: {
+    type: Number,
+    required: true,
+  },
+});
+
+defineEmits(["close"]);
+</script>
