040f259404368bab0dcd3c9ddb0f6c23e202f007
k-majick <studio@cyberleaf.pl>
2024-06-05 17:16:17 +0200

    loader fix

--- components/Loader.vue
+++ components/Loader.vue
@@ -1,30 +1,32 @@
 <template>
-  <div :class="`loader loader--${theme}`">
-    <div class="loader__container">
-      <div class="loader__ray loader__ray--1"></div>
-      <div class="loader__ray loader__ray--2"></div>
-      <div class="loader__ray loader__ray--3"></div>
-      <div class="loader__ray loader__ray--4"></div>
-      <div class="loader__ray loader__ray--5"></div>
-      <div class="loader__ray loader__ray--6"></div>
-      <div class="loader__ray loader__ray--7"></div>
-      <div class="loader__ray loader__ray--8"></div>
-      <div class="loader__ray loader__ray--9"></div>
-      <div class="loader__ray loader__ray--10"></div>
-    </div>
-    <span class="loader__text">{{
-      dataStore.loadError ?
-        $t("messages.loadError") :
-        $t("messages.loading")
-    }}</span>
-    <div
-      v-tooltip="
-        dataStore.loadError
-        ? $t('messages.loadError')
-        : $t('messages.loading')
-      "
-        class="loader__logo"
-      >
+  <div class="loader__wrapper">
+    <div :class="`loader loader--${theme}`">
+      <div class="loader__container">
+        <div class="loader__ray loader__ray--1"></div>
+        <div class="loader__ray loader__ray--2"></div>
+        <div class="loader__ray loader__ray--3"></div>
+        <div class="loader__ray loader__ray--4"></div>
+        <div class="loader__ray loader__ray--5"></div>
+        <div class="loader__ray loader__ray--6"></div>
+        <div class="loader__ray loader__ray--7"></div>
+        <div class="loader__ray loader__ray--8"></div>
+        <div class="loader__ray loader__ray--9"></div>
+        <div class="loader__ray loader__ray--10"></div>
+      </div>
+      <span class="loader__text">{{
+        dataStore.loadError ?
+          $t("messages.loadError") :
+          $t("messages.loading")
+      }}</span>
+      <div
+        v-tooltip="
+          dataStore.loadError
+          ? $t('messages.loadError')
+          : $t('messages.loading')
+        "
+          class="loader__logo"
+        >
+      </div>
     </div>
   </div>
 </template>
