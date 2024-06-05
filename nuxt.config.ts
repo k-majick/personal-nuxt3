7ed15eb6b034ca1b8d8a03a23e41a106e75c8ddd
k-majick <k-majick>
2024-06-05 19:09:51 +0200

    disable vite-plugin-eslint on prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -114,7 +114,7 @@ export default defineNuxtConfig({
     },
   },
   vite: {
-    plugins: [eslint()],
+    plugins: process.env.NODE_ENV === 'production' ? [] : [eslint()],
     css: {
       preprocessorOptions: {
         scss: {
